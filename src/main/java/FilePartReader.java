cb1b0c035ee724a159a08c5407a1e88e1049053b
zBIGniew <zbigniew.piotr.tokarski@gmail.com>
2017-05-17 22:18:07 +0200

    Build catalogue structure, beginning of FilePartReader class.

---
+++ src/main/java/FilePartReader.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ztokarski on 17.05.17.
+ */
+public class FilePartReader {
+    private String filePath;
+    private Integer fromLine;
+    private Integer toLine;
+
+    public FilePartReader() {
+        this.filePath = "";
+        this.fromLine = -1;
+        this.toLine = -1;
+    }
+
+}
---
+++ tests/java/InaccessibleFieldTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ztokarski on 17.05.17.
+ */
+public class InaccessibleFieldTest {
+
+}
