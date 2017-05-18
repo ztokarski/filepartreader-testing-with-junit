a46d956dde95ea7c394d124cd1d91d7e92e7c69b
zBIGniew <zbigniew.piotr.tokarski@gmail.com>
2017-05-18 20:12:23 +0200

    Add 2 tests for setup method

--- tests/java/FilePartReaderTest.java
+++ tests/java/FilePartReaderTest.java
@@ -1,6 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by ztokarski on 17.05.17.
  */
 public class FilePartReaderTest {
 
+    @Test
+    public void testSetupFromLineLT1() {
+        FilePartReader filePartReader = new FilePartReader();
+        assertThrows(IllegalArgumentException.class, () -> {
+            filePartReader.setup("filePath", -1, 2);
+        });
+    }
+
+    @Test
+    public void testSetupToLineLTFromLine() {
+        FilePartReader filePartReader = new FilePartReader();
+        assertThrows(IllegalArgumentException.class, () -> {
+            filePartReader.setup("filePath", 3, 2);
+        });
+    }
 }
+
